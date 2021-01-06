d0cfaab1aa58d11dfb51a5ef3890114d6de7e190
Peter Hartmann <peter.hartmann@emerosn.com>
2021-01-06 01:30:39 -0600

    add holder

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 
 A personal collection of jscad scripts.
 
-## Animation
+  * https://openjscad.org/#https://raw.githubusercontent.com/peter-hartmann/jscad-snippets/main/two-arms.js
+  * https://openjscad.org/#https://raw.githubusercontent.com/peter-hartmann/jscad-snippets/main/parallel.js
+  * https://openjscad.org/#https://raw.githubusercontent.com/peter-hartmann/jscad-snippets/main/armuno2.js
+
+## Animation example
 
 https://openjscad.org/#https://erasta.github.io/openjscad_animation/clock_anim.js
--- armuno2.js
+++ armuno2.js
@@ -13,6 +13,7 @@ function getParameterDefinitions() {
     { name: 'arm1_gap2_angle', type: 'float', initial: 45, caption: "arm1 gap2 angle:", min: 0, max: 90, step: 5 },
     { name: 'joint_radius', type: 'float', initial: 0.8, caption: "joint radius:", min: 0.1, max: 2, step: 0.1 },
     { name: 'joint_width', type: 'float', initial: 2, caption: "joint width:", min: 0.2, max: 6, step: 0.2 },
+    { name: 'holder_length', type: 'float', initial: 4, caption: "holder length:", min: 1, max: 10, step: 0.2 },
   ];
 }
 
@@ -54,7 +55,7 @@ function connectTo(beg, end, angle) {
 }
 
 function main(params) {
-    var {arm1_l,arm1_a,arm2_l,arm2_a,arm1_gap,arm2_gap,arm2_gap_angle,arm1_gap2,arm1_gap2_angle} = params;
+    var {arm1_l,arm1_a,arm2_l,arm2_a,arm1_gap,arm2_gap,arm2_gap_angle,arm1_gap2,arm1_gap2_angle,holder_length} = params;
 
     var robot = [];
 
@@ -62,16 +63,14 @@ function main(params) {
     var servo2 = armFactory(4, 'hotpink', params);
     var arm1a = armFactory(arm1_gap, 'green', params, 0, -1);
     var arm1b = armFactory(arm1_l, 'lightblue', params, 0, 1);
-    var arm2 = armFactory(arm2_l+arm1_gap-params.wideness, 'red', params, 0, 1);
-    // connect the arms at the given angle
+    var arm2a = armFactory(arm2_l+arm1_gap-params.wideness, 'red', params, 0, 1);
     arm1a = connectTo(arm1a, servo2, 90+arm2_a);
     arm1b = connectTo(arm1b, arm1a, 90-arm1_a+arm2_a);
-    arm2 = connectTo(arm2, arm1b, 90+arm1_a-arm2_a);
-    robot.push(servo2, arm1a, arm1b, arm2);
+    arm2a = connectTo(arm2a, arm1b, 90+arm1_a-arm2_a);
+    robot.push(servo2, arm1a, arm1b, arm2a);
 
     var ancor1 = armFactory(4, 'black', params, 5);
     var arm1c = armFactory(arm1_l, 'blue', params, 0, 1);
-    // connect the arms at the given angle
     arm1c = connectTo(arm1c, ancor1, arm1_a);
     robot.push(ancor1, arm1c);
 
@@ -81,7 +80,6 @@ function main(params) {
     var arm2c = armFactory(arm2_gap, 'green', params, 0, -2);
     var arm2cc = armFactory(arm1_gap2, 'green', params, 0, -2);
     var arm2d = armFactory(arm2_l, 'orange', params, 0, -1);
-    // connect the arms at the given angle
     arm1d = connectTo(arm1d, servo1, arm1_a);
     arm2b = connectTo(arm2b, arm1d, -90-arm1_a+arm2_a);
     arm2c = connectTo(arm2c, arm1d, -arm2_gap_angle-arm1_a);
@@ -95,6 +93,14 @@ function main(params) {
     ancor2b = connectTo(ancor2b, ancor2a, arm1_gap2_angle);
     arm1e = connectTo(arm1e, ancor2b, -arm1_gap2_angle+arm1_a);
     robot.push(ancor2a, ancor2b, arm1e);
+
+    var holder1a = armFactory(holder_length, 'yellow', params, 0, -1);
+    var holder1aa = armFactory(arm2_gap, 'yellow', params, 0, -1);
+    var holder1b = armFactory(holder_length, 'yellow', params, 0, 1);
+    holder1aa = connectTo(holder1aa, arm2b, -arm1_gap2_angle+arm2_a);
+    holder1a = connectTo(holder1a, arm2b, arm2_a);
+    holder1b = connectTo(holder1b, arm2a, arm2_a);
+    robot.push(holder1a, holder1aa, holder1b);
 
     robot = addJoints(robot, params);
 
